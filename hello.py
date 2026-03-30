5c965287686261d3f468347972b06d5e555ae346
S <62343069+ferrta@users.noreply.github.com>
2026-03-30 13:41:20 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,4 @@ def greet(name):
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    result = greet("DevOps")
-    print(result)
-    
-    assert result == "Hello, DevOps!"
+    print(greet("DevOps"))
